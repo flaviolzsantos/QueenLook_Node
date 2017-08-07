b02189e22d6b178654d44e0319863cda3f78f737
flaviolzsantos <flaviolzsantos@gmail.com>
2017-08-06 21:12:34 -0300

    prod

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(express.static(__dirname + '/html/src'));
 app.use('/adm', express.static(__dirname + '/html/src/adm'));
 app.use(function (req, res, next) {
     
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-    //res.setHeader('Access-Control-Allow-Origin', 'http://www.queenlook.com.br');    
+    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', 'http://www.queenlook.com.br');    
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
