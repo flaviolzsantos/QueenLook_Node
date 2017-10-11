3c9b5d1d01e89fcd4dd8d57daed42d8bcc353c4e
flaviolzsantos <flaviolzsantos@gmail.com>
2017-10-10 23:17:43 -0300

    build

--- html/src/adm/main.bundle.js
+++ html/src/adm/main.bundle.js
@@ -961,10 +961,16 @@ var _a;
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+// The file contents for the current environment will overwrite these during build.
 var environment = {
-    production: true,
+    production: false,
     basic_auth_token: "aaa",
-    apiEndpoint: "http://www.queenlook.com.br/"
+    //apiEndpoint: "http://www.queenlook.com.br/"
+    apiEndpoint: "http://localhost:3000/"
 };
 //# sourceMappingURL=environment.js.map
 
